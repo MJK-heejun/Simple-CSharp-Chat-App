8397a14a5e57f11e6f7326839042f70ff336aeec
hjk7 <hjk7@sfu.ca>
2016-09-06 23:00:31 -0700

    initial version of chat server

---
+++ ChatServer/ChatServer/HandleClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections;
+
+namespace ChatServer
+{
+    public class HandleClient
+    {
+        TcpClient clientSocket;
+        string clNo;
+        Hashtable clientsList;
+
+        public void startClient(TcpClient inClientSocket, string clineNo, Hashtable cList)
+        {
+            this.clientSocket = inClientSocket;
+            this.clNo = clineNo;
+            this.clientsList = cList;
+            //start new thread for the client 'clineNo'
+            Thread ctThread = new Thread(doChat);
+            ctThread.Start();
+        }
+
+        private void doChat()
+        {
+            int requestCount = 0;
+            byte[] bytesFrom = new byte[clientSocket.ReceiveBufferSize];
+            string dataFromClient = null;
+            Byte[] sendBytes = null;
+            string serverResponse = null;
+            string rCount = null;
+            requestCount = 0;
+
+            while ((true))
+            {
+                try
+                {
+                    requestCount = requestCount + 1;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$$"));
+                    Console.WriteLine("From client - " + clNo + " : " + dataFromClient);
+                    rCount = Convert.ToString(requestCount);
+
+                    Program.broadcastAll(clNo + ">> " + dataFromClient);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    break;
+                }
+            }
+        }
+    } 
+
+
+}
